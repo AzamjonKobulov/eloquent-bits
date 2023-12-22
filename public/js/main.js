ad1d8be4d65087232ac20b95c045acb5812182a2
AzamjonKobulov <azamjonkobulov1998@gmail.com>
2023-12-22 15:20:09 +0500

    add/some improvements

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,3 @@
-// Mobile menu toggling funcs
 const mobileMenu = document.querySelector('.mobile-menu');
 const mobileMenuBtn = document.querySelector('.mobile-menu-btn');
 const mobileMenuCloseBtn = document.querySelector('.mobile-menu-close-btn');
@@ -9,13 +8,35 @@ function toggleMobileMenu() {
   document.body.classList.toggle('overflow-hidden');
 }
 
+function closeMobileMenu() {
+  mobileMenu.classList.add('hidden');
+  document.body.classList.remove('overflow-hidden');
+  mobileMenuLinks.forEach((link) => link.classList.remove('text-white'));
+}
+
 mobileMenuBtn.addEventListener('click', toggleMobileMenu);
-mobileMenuCloseBtn.addEventListener('click', toggleMobileMenu);
+mobileMenuCloseBtn.addEventListener('click', closeMobileMenu);
+
 mobileMenuLinks.forEach((link) => {
-  link.addEventListener('click', toggleMobileMenu);
+  link.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    toggleMobileMenu();
+
+    mobileMenuLinks.forEach((otherLink) =>
+      otherLink.classList.remove('text-white')
+    );
+    link.classList.add('text-white');
+
+    const targetId = link.getAttribute('href').substring(1);
+    const targetSection = document.getElementById(targetId);
+
+    if (targetSection) {
+      targetSection.scrollIntoView({ behavior: 'smooth' });
+    }
+  });
 });
 
-// Tabs Functions
 const tabButtons = document.querySelectorAll('.tab-button');
 
 tabButtons.forEach((button) => {
@@ -28,9 +49,7 @@ tabButtons.forEach((button) => {
     const clickedIcon = button.querySelector('svg');
     clickedIcon.classList.add('active-tab-icon');
 
-    tabButtons.forEach((btn) => {
-      btn.classList.remove('active-tab');
-    });
+    tabButtons.forEach((btn) => btn.classList.remove('active-tab'));
     button.classList.add('active-tab');
 
     const tabId = button.getAttribute('data-tab');
@@ -40,60 +59,36 @@ tabButtons.forEach((button) => {
 
 function showTabContent(tabId) {
   const tabContents = document.querySelectorAll('.tab-content');
-  tabContents.forEach((content) => {
-    content.classList.add('hidden');
-  });
+  tabContents.forEach((content) => content.classList.add('hidden'));
 
   const selectedTabContent = document.getElementById(tabId);
   selectedTabContent.classList.remove('hidden');
 }
 
-// Adding BG color when scrolling.
 const header = document.getElementById('header');
 
 window.addEventListener('scroll', () => {
-  if (window.scrollY > 100) {
-    header.classList.add('bg-brand-green-200/10');
-  } else {
-    header.classList.remove('bg-brand-green-200/10');
-  }
+  header.classList.toggle('bg-brand-green-200/10', window.scrollY > 100);
 });
 
-// Swiper Functions
 document.addEventListener('DOMContentLoaded', function () {
   const mySwiper = new Swiper('.mySwiper', {
     slidesPerView: 1.5,
     spaceBetween: 10,
     loop: true,
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
+    pagination: { el: '.swiper-pagination', clickable: true },
     breakpoints: {
-      0: {
-        slidesPerView: 1.4,
-        spaceBetween: 16,
-      },
-      640: {
-        slidesPerView: 2.5,
-        spaceBetween: 24,
-      },
-      1024: {
-        slidesPerView: 3.5,
-        spaceBetween: 32,
-      },
-      1440: {
-        slidesPerView: 4.15,
-        spaceBetween: 48,
-      },
+      0: { slidesPerView: 1.4, spaceBetween: 16 },
+      640: { slidesPerView: 2.5, spaceBetween: 24 },
+      1024: { slidesPerView: 3.5, spaceBetween: 32 },
+      1440: { slidesPerView: 4.15, spaceBetween: 48 },
     },
   });
 
-  document.getElementById('btn-prev').addEventListener('click', function () {
-    mySwiper.slidePrev();
-  });
-
-  document.getElementById('btn-next').addEventListener('click', function () {
-    mySwiper.slideNext();
-  });
+  document
+    .getElementById('btn-prev')
+    .addEventListener('click', () => mySwiper.slidePrev());
+  document
+    .getElementById('btn-next')
+    .addEventListener('click', () => mySwiper.slideNext());
 });
